b9621bf900ae397c0600c8cfb07f57e2fbf00585
yujin1025 <71168062+yujin1025@users.noreply.github.com>
2024-02-23 23:34:18 +0900

    q skill

--- Source/MiniCyphers/Character/Shiva.cpp
+++ Source/MiniCyphers/Character/Shiva.cpp
@@ -84,85 +84,12 @@ void AShiva::OnUseRightClickAttack()
 
 void AShiva::OnUseShiftLeftClickAttack()
 {
-	APlayerController* PlayerController = Cast<APlayerController>(GetController());
-	if (!PlayerController)
-		return;
-
-	FHitResult HitResult;
-	PlayerController->GetHitResultUnderCursor(ECC_Visibility, false, HitResult);
-
-	if (HitResult.bBlockingHit)
-	{
-		bInvincible = true;
-
-		float TargetHeight = HitResult.ImpactPoint.Z;
-		FVector NewLocation = FVector(HitResult.ImpactPoint.X, HitResult.ImpactPoint.Y, TargetHeight);
-
-		SetActorLocation(NewLocation);
-
-		DrawDebugSphere(GetWorld(), NewLocation, 10.0f, 12, FColor::Green, false, 5.0f); //마우스 클릭한 곳 디버그
-
-
-		ACharacter* HitCharacter = nullptr;
-		const FVector TraceStart = GetActorLocation();
-		const FVector TraceEnd = NewLocation;
-		FHitResult HitResultAfterTeleport;
-		FCollisionQueryParams CollisionParams;
-		CollisionParams.AddIgnoredActor(this); 
-
-		// 레이 캐스트를 통해 순간이동 후 목표 지점에 적이 있는지 확인
-		if (GetWorld()->LineTraceSingleByChannel(HitResultAfterTeleport, TraceStart, TraceEnd, ECC_Pawn, CollisionParams))
-		{
-			HitCharacter = Cast<ACharacter>(HitResultAfterTeleport.GetActor());
-		}
-
-		if (HitCharacter)
-		{
-			UHealthComponent* DamagedHealthComponent = HitCharacter->FindComponentByClass<UHealthComponent>();
-
-			if (DamagedHealthComponent)
-			{
-				// 킬 힐을 맞은 적은 넘어짐 (띄우는 힘이 0인 에어본 타격) 아직 구현안함
-				DamagedHealthComponent->ChangeHealth(this, -10);
-				DrawDebugString(GetWorld(), TraceEnd, FString::Printf(TEXT("10")), nullptr, FColor::Red, 3.0f, false);
-			}
-		}
-
-		bInvincible = false;
-	}
+	
 }
 
 void AShiva::OnUseQSkill()
 {
-	APlayerController* PlayerController = Cast<APlayerController>(GetController());
-	if (!PlayerController)
-		return;
 
-	TArray<FHitResult> HitResults;
-	FVector StartLocation = GetActorLocation();
-	FVector ForwardVector = GetActorForwardVector();
-	FVector EndLocation = StartLocation + ForwardVector * SkillRange;
-
-	FCollisionQueryParams CollisionParams;
-	CollisionParams.AddIgnoredActor(this);
-
-	// 레이 캐스트를 통해 사정거리 안의 모든 적을 확인
-	if (GetWorld()->LineTraceMultiByChannel(HitResults, StartLocation, EndLocation, ECC_Pawn, CollisionParams))
-	{
-		for (const FHitResult& HitResult : HitResults)
-		{
-			ACharacter* HitCharacter = Cast<ACharacter>(HitResult.GetActor());
-			if (HitCharacter)
-			{
-				UHealthComponent* DamagedHealthComponent = HitCharacter->FindComponentByClass<UHealthComponent>();
-				if (DamagedHealthComponent)
-				{
-					DamagedHealthComponent->ChangeHealth(this, -10);
-					break; // 1마리만 타격
-				}
-			}
-		}
-	}
 }
 
 void AShiva::OnUseUltimateSkill()
